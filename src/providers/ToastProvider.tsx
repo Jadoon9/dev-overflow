256cae8d5bdf0b4971750ef4787d3f3dbeeb1e05
Shahzeb Jadoon <shahzebjadoon0@gmail.com>
2025-10-01 01:22:18 +0500

    fixed hydration error

--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -2,9 +2,20 @@
 
 import { Bounce, ToastContainer } from "react-toastify";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const ToastProvider = () => {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <ToastContainer
       position="top-right"
@@ -16,7 +27,7 @@ const ToastProvider = () => {
       pauseOnFocusLoss
       draggable
       pauseOnHover
-      theme={theme}
+      theme={theme === "dark" ? "dark" : "light"}
       transition={Bounce}
     />
   );
